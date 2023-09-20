6356ef9d7c2f59a4547860aa899902c3fdde55dd
KlimJavaDev <klimjavadev@gmail.com>
2023-09-20 18:59:46 +0300

    Створено проєкт Help Desk of Housing Office

---
+++ src/main/java/com/klimjavadev/HelpDeskOfHousingOffice/HelpDeskOfHousingOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.klimjavadev.HelpDeskOfHousingOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDeskOfHousingOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDeskOfHousingOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klimjavadev/HelpDeskOfHousingOffice/HelpDeskOfHousingOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klimjavadev.HelpDeskOfHousingOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskOfHousingOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
